783e8f311e38e9bfcbae32c84064aa9d75144c75
Debora Ramella <debora.ramell@gmail.com>
2024-01-31 18:05:42 +0100

    completed parsing functions

--- secnlp/ml_logic/parsing.py
+++ secnlp/ml_logic/parsing.py
@@ -4,21 +4,17 @@ import string
 import bleach
 from bs4 import BeautifulSoup
 
-regex_expressions = {"7":
-                    {"start": "item\s*[7][\.\;\:\-\_]*\s*\\bM",
-                     "end" : "item\s*7a[\.\;\:\-\_]\sQuanti|item\s*8[\.\,\;\:\-\_]\s*"},
-                    "1" :
-                    {"start" : "item\s*1[\.\;\:\-\_]*\s*\bB\b",
-                     "end" : "item\s*1[abc][\.\,\;\:\-\_]\s*(?:Risk|Unresolved|Cyber)|item\s*2[\.\,\;\:\-\_]\s*Properties"}
-                    }
 
-def parse_10k_10q_filing_item7(text,item = '7'):
+def parse_10k_filing_items(text,item = '7'):
     # Define regex patterns outside the loop for better performance
     if item == '7':
-        item_start = re.compile("item\s*[7][\.\;\:\-\_]*\s*\\bM", re.IGNORECASE)
+        item_start = re.compile("item\s*[7A][\.\;\:\-\_]*\s*\\bM", re.IGNORECASE)
         item_end = re.compile("item\s*7a[\.\;\:\-\_]\sQuanti|item\s*8[\.\,\;\:\-\_]\s*", re.IGNORECASE)
+    if item == '7a':
+        item_start = re.compile(r"Item\s*7A[\s\S]*?\bQu", re.IGNORECASE)
+        item_end = re.compile(r"Item\s*8[\s\S]*?\bFI", re.IGNORECASE)
     elif item == '1':
-        item_start = re.compile("item\s*1[\.\;\:\-\_]*\s*B", re.IGNORECASE)
+        item_start = re.compile("item\s*1[\.\;\:\-\_]*\s*Business", re.IGNORECASE)
         item_end = re.compile("item\s*1[abc][\.\,\;\:\-\_]\s*(?:Risk|Unresolved|Cyber)|item\s*2[\.\,\;\:\-\_]\s*Properties", re.IGNORECASE)
     elif item == '1a':
         item_start = re.compile("item\s*1[a][\.\;\:\-\_]*\s*R", re.IGNORECASE)
@@ -40,25 +36,57 @@ def parse_10k_10q_filing_item7(text,item = '7'):
         item_position = max(positions, key=lambda p: p[1] - p[0])
         return text[item_position[0]:item_position[1]]
     else:
-        print(f"Unable to locate Item 1")
+        print(f"Unable to locate Item {item}")
+        return None
+
+
+def parse_10q_filing_items(text,item = '7'):
+    # Define regex patterns outside the loop for better performance
+    if item == '2':
+        item_start = re.compile(r"Item\s*2[\s\S]*?\bMa", re.IGNORECASE)
+        item_end = re.compile(r"Item\s*3[\s\S]*?\bQu", re.IGNORECASE)
+    if item == '1a':
+        item_start = re.compile(r"Item\s*1A\.[\s\S]*?(?=\bRisk Factors\b)", re.IGNORECASE)
+        item_end = re.compile(r"Item\s*5\.[\s\S]*?(?=\bOther Information\b)", re.IGNORECASE)
+
+    # Find all start and end positions using finditer
+    starts = [i.start() for i in item_start.finditer(text)]
+    ends = [i.start() for i in item_end.finditer(text)]
+
+    positions = []
+    for s in starts:
+        nearest_end = min((e for e in ends if e > s), default=None)
+        if nearest_end is not None:
+            positions.append([s, nearest_end])
+
+    # Check if positions is not empty before using max
+    if positions:
+        # Use max() with a key function to find the position with the maximum length
+        item_position = max(positions, key=lambda p: p[1] - p[0])
+        return text[item_position[0]:item_position[1]]
+    else:
+        print(f"Unable to locate Item {item}")
         return None
 
 def cleaning(text):
-    # $CHALLENGIFY_BEGIN
     # Replace new lines
     text = text.replace('\n', ' ')
+    # Removing HTML tags
+    text = bleach.clean(text, tags=[], strip=True)
+    # Removing URLs
+    text = re.compile(r'https?://\S+|www\.\S+').sub('', text)
     # Removing whitespaces
     text = text.strip()
     # Lowercasing
     text = text.lower()
-    # Removing numbers
-    text = ''.join(char for char in text if not char.isdigit())
     # Removing contractions
     text = contractions.fix(text)
-    # Removing HTML tags
-    text = bleach.clean(text, tags=[], strip=True)
     # Removing punctuation
     for punctuation in string.punctuation:
         text = text.replace(punctuation, '')
+        # Normalize whitespace
+    text = re.sub(r'\s+', ' ', text)
+        # Removing irrelevant characters
+    text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
 
     return text
